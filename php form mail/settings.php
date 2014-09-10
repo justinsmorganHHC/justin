ba157cde77cdd4ab435088f864637fd72f27aa08
justinsmorgan <justinsmorgan@gmail.com>
2014-09-10 15:05:00 -0400

    Merge branch 'master' of https://github.com/justinsmorganHHC/justin
    Conflicts:
    index.html

---
+++ php form mail/settings.php
@@ -0,0 +1,17 @@
+<?php
+
+/* Settings
+---------------------*/
+$email_to = 'application@prorepuniversity.com'; // Email addresses to send data. Seperate email addresses with ','
+$site_title = 'prorepu.com'; // Your website title
+$webmaster_name = "Pro Rep University"; // Your name
+
+$headerbar_color = "#313131"; // Color of the headerbar in the email sent
+$form_type = "contact form"; // Type of form - default is Contact Form
+
+/* Uncomment line below if you want to redirect user to another page upon submission.
+If not set, the script will return data with a string: 'success'
+----------------------------------------------------*/
+$success_redirect = "https://www.prorepu.com/thank_you.html"; // Page to redirect to after submission of form.
+
+?>
