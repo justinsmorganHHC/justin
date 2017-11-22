0fb461e3781e6a9ee18e884c3a278a8188f7b1d3
justin.s.morgan <justin.s.morgan@outlook.com>
2017-11-22 11:45:23 -0500

    Update settings.php

--- php form mail/settings.php
+++ php form mail/settings.php
@@ -2,9 +2,9 @@
 
 /* Settings
 ---------------------*/
-$email_to = 'application@prorepuniversity.com'; // Email addresses to send data. Seperate email addresses with ','
-$site_title = 'prorepu.com'; // Your website title
-$webmaster_name = "Pro Rep University"; // Your name
+$email_to = 'application@0-0x.com.com'; // Email addresses to send data. Seperate email addresses with ','
+$site_title = '0-0x.com.com'; // Your website title
+$webmaster_name = "Justin Morgan"; // Your name
 
 $headerbar_color = "#313131"; // Color of the headerbar in the email sent
 $form_type = "contact form"; // Type of form - default is Contact Form
@@ -12,6 +12,6 @@ $form_type = "contact form"; // Type of form - default is Contact Form
 /* Uncomment line below if you want to redirect user to another page upon submission.
 If not set, the script will return data with a string: 'success'
 ----------------------------------------------------*/
-$success_redirect = "https://www.prorepu.com/thank_you.html"; // Page to redirect to after submission of form.
+$success_redirect = "https://0-0x.com/sample/thank_you.html"; // Page to redirect to after submission of form.
 
 ?>
